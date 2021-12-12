217590da1b281ff5080a345fae7253dce010deed
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-12 15:50:27 +0300

    Add files via upload

---
+++ consoleTask_9_2/consoleTask_9_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using consoleTask_9;
+namespace consoleTask_9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2 t = new Task2();
+            Console.WriteLine("Считано:\n{0}\n", t.Str);
+            t.F();
+            t.Write();
+            Console.WriteLine("Изменено:\n{0}", t.Str);
+        }
+    }
+}
