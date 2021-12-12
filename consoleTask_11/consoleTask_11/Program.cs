ccfd476c91ce434c4d936024c7ea952b3d8bea7c
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-12 15:51:34 +0300

    Add files via upload

---
+++ consoleTask_11/consoleTask_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace consoleTask_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                Class1 o = new Class1(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Вводите числа через пробел: ");
+                o.Input(Console.ReadLine());
+                o.Sort();
+                Console.WriteLine("Сортированный массив: {0}", o.Show());
+                Console.WriteLine("Размер массива: {0}", o.Length);
+                Console.Write("Введите скаляр, на который умножить: ");
+                o.Skal = int.Parse(Console.ReadLine());
+                Console.WriteLine("Массив: {0}", o.Show());
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
