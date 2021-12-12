cccd31a1807950db438670c8b038c9c9632dce40
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-12 15:48:48 +0300

    Add files via upload

---
+++ consoleTask_8/consoleTask_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace consoleTask_8
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Вводите слова через пробел: ");
+                string str = Console.ReadLine();
+                Class1 o = new Class1(str);
+                Console.WriteLine("Результат:\n{0}", o.Result);
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            
+        }
+    }
+}
---
+++ consoleTask_9_1/consoleTask_9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using Task_9;
+
+
+namespace consoleTask_9_1
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string str = Console.ReadLine();
+            Task_1 obj = new Task_1(str);
+            obj.Write();
+            string result = obj.Read();
+            Console.WriteLine(result);
+        }
+    }
+}
