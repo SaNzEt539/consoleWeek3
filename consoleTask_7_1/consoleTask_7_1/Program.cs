a190dcc23d66fdd0cf9e3a73849c513112b1dafc
SaNzEt539 <95311995+SaNzEt539@users.noreply.github.com>
2021-12-12 15:47:41 +0300

    Add files via upload

---
+++ consoleTask_7_1/consoleTask_7_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace consoleTask_7_1
+{
+    class Task
+    {
+        string str;
+        public Task(string str)
+        {
+            this.str = str;
+        }
+        public string Ans()
+        {
+            string ans = String.Empty;
+            for (int i = 0; i<str.Length; i++)
+            {
+                if (str[i]!=':')
+                {
+                    ans += str[i];
+                }
+                else
+                {
+                    return ans;
+                }
+            }
+            return ans;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            try
+            {
+                Task t = new Task(str);
+                Console.WriteLine("Результат: {0}", t.Ans());
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то!");
+            }
+        }
+    }
+}
---
+++ consoleTask_7_2/consoleTask_7_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+
+namespace consoleTask_7_2
+{
+    class Task
+    {
+        StringBuilder str;
+        public Task(string str)
+        {
+            this.str = new StringBuilder(str);
+            Ans();
+        }
+        void Ans()
+        {
+            int i = 0;
+            while(i<str.Length-1) 
+            {
+                switch(str[i])
+                {
+                    case ' ':
+                       switch(str[i+1])
+                        {
+                            case '?':
+                            case '!':
+                            case ',':
+                            case '.':
+                                str.Remove(i, 1);
+                                i--;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case '?':
+                    case '!':
+                    case ',':
+                    case '.':
+                        if (str[i + 1] == ' ' ) 
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            str.Insert(i+1, ' ');
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                i++;
+            }
+        }
+        public string Show()
+        {
+            return str.ToString();
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            Task t = new Task(Console.ReadLine());
+            Console.WriteLine("Результат:\n{0}", t.Show());
+        }
+    }
+}
